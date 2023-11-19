9464b5d1af2e2f4aceb1cf8361a1fb7a5ba0a135
Tyuiu.KarpenkoNA@outlook.com <karpenko.nik05@mail.ru>
2023-11-19 23:42:48 +0500

    Task2

---
+++ Tyuiu.KarpenkoNA.Sprint5.Task2.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KarpenkoNA.Sprint5.Task2.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KarpenkoNA.Sprint5.Task2.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("71ee7f78-f01c-4bb6-823b-eef2ff5a7af1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.KarpenkoNA.Sprint5.Task2.V21/Program.cs
+++ Tyuiu.KarpenkoNA.Sprint5.Task2.V21/Program.cs
@@ -4,45 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.KarpenkoNA.Sprint5.Task1.V26.Lib;
+using Tyuiu.KarpenkoNA.Sprint5.Task2.V21.Lib;
 
-namespace Tyuiu.KarpenkoNA.Sprint5.Task1.V26
+namespace Tyuiu.KarpenkoNA.Sprint5.Task2.V21
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int startValue = -5;
-            int stopValue = 5;
+            int[,] mtrx = new int[3, 3] { { 4, 8, 5 },
+                                          { 6, 3, 2 },
+                                          { 9, 4, 5 } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
             DataService ds = new DataService();
 
             Console.Title = "Спринт #5 | Выполнил: Карпенко Н. А. | АСОиУБ-23-2";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
-            Console.WriteLine("* Задание #1                                                              *");
-            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
             Console.WriteLine("* Выполнил: Карпенко Никита Андреевич | АСОиУБ-23-2                       *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дана функция,(произвести табулирование) f(x) на заданном диапазоне      *");
-            Console.WriteLine("* [-5; 5] с шагом 1. Произвести проверку деления на ноль. При делении на  *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
             Console.WriteLine("* ноль вернуть значение 0. результат сохранить в текстовый файл           *");
-            Console.WriteLine("* OutPutFileTask1.txt и вывести на консоль в таблицу.Значения округлить   *");
-            Console.WriteLine("* до двух знаков после запятой.                                           *");
+            Console.WriteLine("* Результат сохранить в файл OutPutFileTask2.csv и вывести на консоль     *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("startValue = " + startValue);
-            Console.WriteLine("stopValue = " + stopValue);
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            string res = ds.SaveToFileTextData(startValue, stopValue);
+            string res = ds.SaveToFileTextData(mtrx);
 
             Console.WriteLine("Файл: " + res);
             Console.WriteLine("Создан!");
