fa2c9a30918a80be5effe400276e14ae4b02238e
Tyuiu.KarpenkoNA@outlook.com <karpenko.nik05@mail.ru>
2023-11-24 18:35:46 +0500

    Task5

---
+++ Tyuiu.KarpenkoNA.Sprint5.Task5.V27/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.KarpenkoNA.Sprint5.Task5.V27.Lib;
+
+namespace Tyuiu.KarpenkoNA.Sprint5.Task5.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Карпенко Н. А. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Карпенко Никита Андреевич | АСОиУБ-23-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:/DataSprint5/InPutDataFileTask5V27.txt (файл взять из       * ");
+            Console.WriteLine("* архива согласно вашему варианту. Создать папку в ручную                 *");
+            Console.WriteLine("* С:/DataSprint5/ и скопировать в неё файл) в котором есть набор          *");
+            Console.WriteLine("* значений. Найти среднее значение всех целых чисел в файле, которые      *");
+            Console.WriteLine("* делятся на 5.Полученный результат вывести на консоль.                   *");
+            Console.WriteLine("* У вещественных значений округлить до трёх знаков после запятой.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V27.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Среднее значение = " + res);
+            Console.ReadKey();
+        }
+    }
+}
